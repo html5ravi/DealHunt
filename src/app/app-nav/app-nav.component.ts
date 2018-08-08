853952c8d30afb3e340e1d4805a5f3628bc3d72b
Ravi G <ravi.g@spurtreetech.com>
2018-08-08 17:16:15 +0530

    Sidenav functionality are done

--- src/app/app-nav/app-nav.component.ts
+++ src/app/app-nav/app-nav.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import {MatSidenav} from '@angular/material/sidenav';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +11,21 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app-nav.component.scss'],
 })
 export class AppNavComponent {
-
+ 
+  @ViewChild(MatSidenav) drawer: MatSidenav;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private _router:Router) {}
+
+    ngOnInit() {
+      this._router.events.subscribe(() => {
+        
+          this.drawer.close();
+        
+      });
+    }
   
   }
