74dcfd70c197706508cef5ce3c3c36c031a297a7
Ravi G <ravi.g@spurtreetech.com>
2018-08-21 17:38:48 +0530

    Done Routing, Signup page validation functionalities

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+export interface Gender {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,7 +17,10 @@ import { ReactiveFormsModule, FormControl, FormsModule, FormGroup, FormBuilder, 
 export class SignupComponent implements OnInit {
   public signUp_form:FormGroup;
   public Food:any;
-  constructor( public formBuilder:FormBuilder) {
+  public Genders:any;
+  loading = false;
+  submitted = false;
+  constructor( public formBuilder:FormBuilder, public router:Router) {
    
    }
 
@@ -24,14 +35,30 @@ export class SignupComponent implements OnInit {
   });
   }
 
-  foods: Food[] = [
+  cities: Food[] = [
     {value: 'bangalore-0', viewValue: 'Bangalore'},
     {value: 'chennai-1', viewValue: 'Chennai'},
     {value: 'Delhi-2', viewValue: 'Delhi'}
   ];
+  genders: Gender[] = [
+    {value: 'male', viewValue: 'Male'},
+    {value: 'female', viewValue: 'Female'}
+  ];
+  
+  get f() { return this.signUp_form.controls; }
+
+  onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.signUp_form.invalid) {
+            return;
+        }
+
+        // this.loading = true;
+        this.router.navigate(['/register2']);
+    }
+
 }
-export interface Food {
-  value: string;
-  viewValue: string;
-}
+
 
