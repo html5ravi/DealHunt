e81d0b7e9868fca1fc6270410d68ded3b97ee3c5
Ravi G <ravi.g@spurtreetech.com>
2018-08-30 12:35:07 +0530

    done environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://api.sttarter.com/contentsystem/',
+  production: false,  
+  authURL:'http://sol-nightly.capillary.in/auth/v1/',
+  apiURL:'http://sol-qa.capillarytech.com/mobile/v2/api/',
+  sttarterURL:'http://api.sttarter.com/contentsystem/',
   token:'cea94caf3b38196b8f0f5cea05a534c9/'
-  // apiUrl: 'http://localhost:4000'
 };
