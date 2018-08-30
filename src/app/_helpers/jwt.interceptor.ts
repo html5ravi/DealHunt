9c6a52c73b15e023a64a3c5379d5d29e866552e5
Ravi G <ravi.g@spurtreetech.com>
2018-08-30 13:05:09 +0530

    Fixed preflight issue

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment as env} from '../../environments/environment';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+    
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         // let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         let token = JSON.parse(localStorage.getItem("token"));
         let mobile = JSON.parse(localStorage.getItem("mobile"));
-        // console.log(token, mobile)
-        if(token) {
+        
+        if(token && !env.sttarterURL) {
             request = request.clone({
                 setHeaders: { 
                     'cap_authorization':token,
